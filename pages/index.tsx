0822e720e4abb6be4c76ce2b7f4b52128de55398
augustoyuudi <augustoyuudi@gmail.com>
2022-04-11 15:09:38 -0300

    feat: call header component in home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import type { NextPage } from 'next'
+import { Header } from "@/components";
 
 const Home: NextPage = () => {
   return (
     <div>
+      <Header></Header>
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
